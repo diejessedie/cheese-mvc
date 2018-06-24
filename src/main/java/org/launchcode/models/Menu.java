a2dc59c98d63da40484d2eaaf3cdbe74ea70a6af
diejessedie <jjohnson4927@gmail.com>
2018-06-23 20:06:34 -0500

    Assignment finished

--- src/main/java/org/launchcode/models/Menu.java
+++ src/main/java/org/launchcode/models/Menu.java
@@ -1,16 +1,16 @@
 package org.launchcode.models;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
-import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by LaunchCode
- */
 @Entity
-public class Category {
+public class Menu {
+
 
     @Id
     @GeneratedValue
@@ -20,15 +20,20 @@ public class Category {
     @Size(min=3, max=15, message = "Name cannot be empty")
     private String name;
 
-    @OneToMany
-    @JoinColumn(name = "category_id")
-    private List<Cheese> cheeses = new ArrayList<>();
 
-    public Category() {}
+    @ManyToMany
+    private List<Cheese> cheeses;
 
-    public Category(String name) {
+    public Menu(String name) {
         this.name = name;
     }
+
+    public Menu() { }
+
+    public void addItem(Cheese item) {
+        cheeses.add(item);
+    }
+
 
     public int getId() {
         return id;
